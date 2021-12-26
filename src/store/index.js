7279f30660fbfd12ddfcd7af85614d8c525e2479
Anastasia Maryina <nastiacool55@gmail.com>
2021-12-26 16:29:47 +0300

    store enhancer added

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,26 @@ import { heroesReducer, filtersReducer } from "../reducers";
 
 const reducers = { heroes: heroesReducer, filters: filtersReducer };
 
-const store = createStore(
-	combineReducers(reducers),
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const enhancer =
+	(createStore) =>
+	(...args) => {
+		const store = createStore(...args);
+
+		const oldDispatch = store.dispatch;
+		store.dispatch = (action) => {
+			if (typeof action === "string") {
+				return oldDispatch({
+					type: action,
+				});
+			}
+			return oldDispatch(action);
+		};
+
+		return store;
+	};
+
+const store = createStore(combineReducers(reducers), enhancer);
 
 export default store;
+
+//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
